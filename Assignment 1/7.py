07abb9967c4d5386bcf2323af1d7dfdfb310b76b
caophongnt <caophongnt@example.com>
2026-01-15 15:03:32 +0700

    ...

---
+++ Assignment 1/7.py
@@ -0,0 +1,14 @@
+import random
+#combination lock
+lock_number1 = random.randint(a=0, b=9)
+lock_number2 = random.randint(a=0, b=9)
+lock_number3 = random.randint(a=0, b=9)
+
+print(lock_number1, lock_number2, lock_number3)
+
+lock_number4 = random.randint(a=1, b=6)
+lock_number5 = random.randint(a=1, b=6)
+lock_number6 = random.randint(a=1, b=6)
+lock_number7 = random.randint(a=1, b=6)
+
+print(lock_number4, lock_number5, lock_number6, lock_number7)
