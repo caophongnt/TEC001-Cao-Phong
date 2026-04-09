8d863c718d947aad7122c022bc1b96b3a83c96b9
Kphonga <kphongaok@gmail.com>
2026-04-09 13:27:59 +0700

    Update 5.py

---
+++ Assignment 6/5.py
@@ -0,0 +1,14 @@
+def remove_odds(numbers):
+    even_numbers = []
+    for num in numbers:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+
+def main(): 
+    my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    result = remove_odds(my_numbers)
+    print("Original list:", my_numbers)
+    print("List without odd numbers:", result)
+if __name__ == "__main__":
+    main()
